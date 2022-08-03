dd2b79033e653d166317c0daa0bc057cef527177
Lex <Lexor.V.A.S@yandex.ru>
2022-08-04 00:44:12 +0300

    add cards error

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,11 +1,13 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Card from '../models/card';
-export const getCards = (req: Request, res: Response) => {
+const NotFoundError = require('../errors/notFoundError');
+const BadRequestError = require('../errors/badRequestError');
+export const getCards = (req: Request, res: Response, next: NextFunction) => {
   return Card.find({})
     .then((Card) => res.send({ Card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(next);
 }
-export const createCard = (req: any, res: Response) => {
+export const createCard = (req: any, res: Response, next: NextFunction) => {
   const { name, link } = req.body;
   return Card.create({
     name: name,
@@ -13,14 +15,25 @@ export const createCard = (req: any, res: Response) => {
     owner: req.user._id
   })
     .then((card) => res.send({ card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestError('Неправильно заполнено поле');
+      }
+      else return next(err)
+    })
+    .catch(next);
 }
-export const deleteCard = (req: Request, res: Response) => {
+export const deleteCard = (req: Request, res: Response, next: NextFunction) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({card}))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Нет карты с указанным id');
+      }
+      res.send(card)
+    })
+    .catch(next);
 }
-export const likeCard = (req: any, res: Response) => {
+export const likeCard = (req: any, res: Response, next: NextFunction) => {
   Card.findByIdAndUpdate(req.params.cardId,
     {
       $addToSet:
@@ -28,15 +41,25 @@ export const likeCard = (req: any, res: Response) => {
     },
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      res.send(card)
+      if (!card) {
+        throw new BadRequestError('Неправильный id');
+      }
+    })
+    .catch(next);
 }
-export const dislikeCard = (req: any, res: Response) => {
+export const dislikeCard = (req: any, res: Response, next: NextFunction) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-  .then((card) => res.send({ data: card }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      res.send(card)
+      if (!card) {
+        throw new BadRequestError('Неправильный id');
+      }
+    })
+    .catch(next);
 }
