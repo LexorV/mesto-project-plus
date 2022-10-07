df6347d1f86c6d2bf2d6fb285275e0def58d6dc3
Lex <Lexor.V.A.S@yandex.ru>
2022-10-07 18:54:07 +0300

    fix cards

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -10,8 +10,7 @@ import {
 import { UrlPicture, IDValid } from '../constants/RegularConst';
 
 const router = Router();
-
-router.get('/cards', getCards);
+router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
